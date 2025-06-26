c4492dec4a301946ec6682dd0d0ff2a679371c8b
Srinanda <srinandakv2004@gmail.com>
2025-06-26 22:39:16 +0300

    fixed bug

--- util/datasets.py
+++ util/datasets.py
@@ -32,15 +32,12 @@ class IP102(torch.utils.data.Dataset):
             lines = f.readlines()
 
         for line in lines:
-	    filename, class_id = line.strip().split()
-	    class_id = int(class_id)
+            filename, class_id = line.strip().split()
+            class_id = int(class_id)
 
-	    # Add class_id as subfolder to match dataset structure
-	    filename = os.path.join(str(class_id), filename)
-
-	    self.image_list.append(os.path.join(root, split, filename))
-	    self.label_list.append(class_id)
-
+    
+            self.image_list.append(os.path.join(root, split, filename))
+            self.label_list.append(class_id)
 
         self.transform = transform
 
