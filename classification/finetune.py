69a851069a5608dca40a6c1c2a3557e55ce38639
Srinanda <srinandakv2004@gmail.com>
2025-06-26 21:23:11 +0300

    Modified finetune.py

--- classification/finetune.py
+++ classification/finetune.py
@@ -24,7 +24,7 @@ import torchvision.transforms as transforms
 
 import timm
 
-assert timm.__version__ == "0.3.2" # version check
+# assert timm.__version__ == "0.3.2" # version check
 from timm.models.layers import trunc_normal_
 from timm.data.mixup import Mixup
 from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
