802e92071d33358f42bd71ef267236b698e7a79c
Srinanda <srinandakv2004@gmail.com>
2025-06-26 22:06:27 +0300

    model_state_dict

--- classification/finetune.py
+++ classification/finetune.py
@@ -248,7 +248,7 @@ def main(args):
         checkpoint = torch.load(args.finetune, map_location='cpu')
 
         print("Load pre-trained checkpoint from: %s" % args.finetune)
-        checkpoint_model = checkpoint['model']
+        checkpoint_model = checkpoint['model_state_dict']
         new_state_dict = {}
         state_dict = model.state_dict()
         for k in checkpoint_model:
